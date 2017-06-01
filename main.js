ff688c8b321d6262de187559663d8f117ab7cde9
Dag Frode Solberg <dag.frode.solberg@gmail.com>
2017-06-02 00:15:04 +0200

    Updated port

--- main.js
+++ main.js
@@ -133,6 +133,6 @@ app.get('/', function (req, res) {
             parseInt(req.query.thickness)));
 });
 
-app.listen(3000, function () {
+app.listen(3001, function () {
     console.log('Example app listening on port 3000!')
 });
